269e29311a0607407de8adfbdcfaa5843721d534
wisdman <wisdman@ajaw.it>
2016-10-28 11:56:57 +0500

    Add fullscreen mode

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ const electron = require('electron')
 const BrowserWindow = electron.BrowserWindow
 const app = electron.app
 
-const debug = /--debug/.test(process.argv[2])
+const debug = /--debug/.test(process.argv.join())
+const fullscreen = /--fullscreen/.test(process.argv.join())
 
 if (process.mas)
 	app.setName('Turing test game')
@@ -20,7 +21,7 @@ app.on('ready', () => {
 	let electronScreen = electron.screen;
 	let size = electronScreen.getPrimaryDisplay().workAreaSize
 
-	let mainWindow = new BrowserWindow({
+	let options = Object.assign({
 		title: app.getName(),
 		width: size.width,
 		height: size.height,
@@ -29,14 +30,16 @@ app.on('ready', () => {
 		resizable: false,
 		movable: false,
 		minimizable: false,
-		maximizable: false,
+		maximizable: false
+	}, fullscreen && {
+		fullscreen: true,
+		kiosk: true,
+		closable: false,
+		alwaysOnTop: true,
+		titleBarStyle: 'hidden'
+	} || {})
 
-		// fullscreen: true,
-		// kiosk: true,
-		// closable: false,
-		// alwaysOnTop: true,
-		// titleBarStyle: 'hidden'
-	})
+	let mainWindow = new BrowserWindow(options)
 
 	mainWindow.loadURL(path.join('file://', process.cwd(), '/game/game.html'))
 
